b086a7bf26c73925d60754d9f0137c17bf7b3c58
roberto_santos_yela <roberto_santos_apps1ma1819@cev.com>
2020-01-21 01:45:17 +0100

    api_total_usage_time_per_day_route_added

--- routes/api.php
+++ routes/api.php
@@ -40,12 +40,16 @@ Route::group(['middleware' => ['auth']], function () {
     Route::post('/store_apps_data', 'AppController@store_apps_data'); 
     Route::get('/get_apps_data', 'AppController@get_apps_data');  
     Route::get('/total_usage_time/{id}', 'AppController@total_usage_time');
-    Route::get('/total_usage_time_beta/{id}', 'AppController@total_usage_time_beta');
+    Route::get('/total_usage_time_beta/{id}', 'AppController@total_usage_time_beta');   
+    
     
 
     Route::get('/get_app_details', 'AppController@get_app_details');
     Route::get('/get_app_statistics', 'AppController@get_app_statistics');
     Route::get('/get_app_coordinates/{app_name}/{app_date}', 'AppController@get_app_coordinates');
+
+
+    Route::get('/total_usage_time_per_day', 'AppController@total_usage_time_per_day');   
 
 });
 
