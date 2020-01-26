d6f625e5b92e174d54708d60b74d6e167113c02e
roberto_santos_yela <roberto_santos_apps1ma1819@cev.com>
2020-01-26 23:52:44 +0100

    api_new_routes_added

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::post('/recover_user_password', 'UserController@recover_user_password');
 Route::get('/generate_password', 'UserController@generate_password');
 
 //PRUEBAS
-Route::post('/store_apps_list', 'AppController@store_apps_list'); 
+
 
 Route::group(['middleware' => ['auth']], function () {
 
@@ -39,20 +39,31 @@ Route::group(['middleware' => ['auth']], function () {
         
     Route::apiResource('/app', 'AppController'); 
     
-    Route::post('/store_apps_data', 'AppController@store_apps_data'); 
+    
     Route::get('/get_apps_data', 'AppController@get_apps_data');  
    
     Route::get('/total_usage_time/{id}', 'AppController@total_usage_time');
-    Route::get('/total_usage_time_beta/{id}', 'AppController@total_usage_time_beta');   
+    Route::get('/total_usage_time_beta/{id}', 'AppController@total_usage_time_beta');  
+
+    Route::get('/get_app_total_usage_time/{id}', 'AppController@get_app_total_usage_time'); 
     
-    
+       
+    Route::post('/store_apps_list', 'AppController@store_apps_list');
+    Route::post('/store_apps_data', 'AppController@store_apps_data');  
 
     Route::get('/get_app_details', 'AppController@get_app_details');
     Route::get('/get_app_statistics', 'AppController@get_app_statistics');
-    Route::get('/get_app_coordinates/{app_name}/{app_date}', 'AppController@get_app_coordinates');
+    Route::get('/get_apps_coordinates', 'AppController@get_apps_coordinates');
+    
+     
+    Route::get('/get_apps_statistics', 'AppController@get_apps_statistics');
 
 
-    Route::get('/total_usage_time_per_day', 'AppController@total_usage_time_per_day');   
+    Route::get('/prueba', 'AppController@prueba');
+    
+     
+
+
 
 });
 
