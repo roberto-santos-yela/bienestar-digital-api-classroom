365104070b1697eb525f5caba9b3279ec713a1d0
ROBERTO_SANTOS APPS1MA1819 <roberto_santos_apps1ma1819@cev.com>
2020-02-06 14:13:39 +0100

    emails_notification_mail_blade_added

---
+++ resources/views/emails/notification_mail.blade.php
@@ -0,0 +1,5 @@
+<p>Buenas <strong>{{ $name }}</strong>,</p>
+
+<p>Has excedido su tiempo de uso en la aplicación de <strong>{{ $app_name }}</strong>.</p>
+
+<p>Atentamente, el equipo de <strong>Bienestar Digital</strong>.</p>
